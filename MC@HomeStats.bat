0415926db10ba81a773c2f07e393806cb040c34e
Are4 <41898367+Are4@users.noreply.github.com>
2020-08-03 22:48:58 +0300

    Main files

---
+++ MC@HomeStats.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+python webscrap.py
+pause
---
+++ webscrap.py
@@ -0,0 +1,91 @@
+import requests
+from bs4 import BeautifulSoup
+from string import digits
+
+refrnr = 0
+tryagain = 1
+fr = open("webscrapuid", "r")
+uidpreset = fr.read()
+fr.close()
+fw = open("webscrapuid", "w")
+fw2 = open("webscrapuid", "w")
+
+if uidpreset == "":
+    uid = int(input("Enter a valid UID: "))
+    struid = str(uid)
+    savechoice = int(input("Type 1 to save UID for automatic use (can change later): "))
+    if savechoice == 1:
+        open("webscrapuid", "w")
+        fw.truncate(0)
+        fw.write(struid)
+        fw.close()
+        print("UID was successfully saved")
+    else:
+        print("UID not saved")
+else:
+    uidpreset = ''.join(c for c in uidpreset if c in digits)
+    struid = uidpreset
+
+while True:
+    print("Refresh attempt no.", str(refrnr))
+    url = "https://minecraftathome.com/minecrafthome/show_user.php?userid=" + struid
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    info = soup.findAll(style="padding-left:12px")[3]
+    result = str(info)
+    total_creds = ""
+    for i in range(len(result)):
+        if i != 10:
+            total_creds = total_creds + result[i]
+    total_creds2 = ""
+    for i in range(len(total_creds)):
+        if i != 27:
+            total_creds2 = total_creds2 + total_creds[i]
+    not_allowed_list = ["<td style=padding-left:12px>", "</td>"]
+    for not_allowed_list_item in not_allowed_list:
+        total_creds2 = total_creds2.replace(not_allowed_list_item, "")
+    info2 = soup.findAll(style="padding-left:12px")[4]
+    result2 = str(info2)
+    total_creds3 = ""
+    for i in range(len(result2)):
+        if i != 10:
+            total_creds3 = total_creds3 + result2[i]
+    total_creds4 = ""
+    for i in range(len(total_creds3)):
+        if i != 27:
+            total_creds4 = total_creds4 + total_creds3[i]
+    not_allowed_list = ["<td style=padding-left:12px>", "</td>"]
+    for not_allowed_list_item in not_allowed_list:
+        total_creds4 = total_creds4.replace(not_allowed_list_item, "")
+    print("Total credits for UID [", struid, "]: ", total_creds2, sep="")
+    print("Total recent average credits for UID [", struid, "]: ", total_creds4, sep="")
+    print("")
+    while tryagain == 1:
+        optend = input("Type 'r' to refresh with same UID or 'a' to input and/or save another UID: ")
+        if optend == "r":
+            refrnr = refrnr + 1
+            print(" ")
+            print("Refreshing...")
+            print(" ")
+            break
+        else:
+            if optend == "a":
+                refrnr = 0
+                print(" ")
+                truncatechoice = int(input("Type 1 if you want to save another UID (or not to keep the old one but search"
+                                            " a new one): "))
+                if truncatechoice == 1:
+                    uid = int(input("Enter new UID: "))
+                    struid = str(uid)
+                    open("webscrapuid", "w")
+                    fw2.truncate(0)
+                    fw2.write(struid)
+                    fw2.close()
+                    print("New uid successfully saved")
+                else:
+                    uid = int(input("Enter new UID: "))
+                    struid = str(uid)
+                print(" ")
+                break
+            else:
+                tryagain = 1
